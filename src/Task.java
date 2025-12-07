ed7b67dd3f1fec1d9dfa9930f8905c8ff3ecebb5
silaoangelica <silaoangelicamay@gmail.com>
2025-12-07 15:28:51 +0800

    added model class

--- src/Task.java
+++ src/Task.java
@@ -1,8 +1,45 @@
+/* this file is the database, the model file. defines the data being used by the system
+*/
+
 public class Task {
 
+    private int id;
+    private String description;
+    private boolean completed;
 
-    public Task(){
-
+    public Task(int id, String description){
+        this.id = id; //changeable? no, so setter not needed
+        this.description = description; //changeable? yes, setter needed
+        this.completed = false; // changeable? yes, setter needed. no need to declare on Task parameters because we are setting the default value to false so we can change it later
     }
+
+    //setter
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public void setCompleted(boolean completed){
+        this.completed = completed;
+    }
+
+    //getter
+    public int getId(){
+        return id;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public boolean getCompleted(){
+        return completed;
+    }
+
+    //use string
+    //expected format: [x] 1: TestDescription
+    public String toString(){
+        return String.format("[%s], %d : %s ", completed ? "x" : " ", id, description);
+    }
+
 
 }
