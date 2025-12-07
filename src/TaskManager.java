c3a55631801f0a4fa2091e50f3c51f6c4647bb18
silaoangelica <silaoangelicamay@gmail.com>
2025-12-07 16:56:50 +0800

    added repository model

---
+++ src/TaskManager.java
@@ -0,0 +1,74 @@
+/*This is the repository file
+* Handles CRUD management
+* */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskManager {
+
+    private List<Task> tasks;
+
+    //need to add because we are creating a new id for every new description
+    private int id = 1;
+
+    public TaskManager(){
+        this.tasks = new ArrayList<>();
+    }
+
+    /**
+     * Creates a new Task with a unique ID and the given description,
+     * adds it to the task list, and returns the created Task.
+     *
+     * @param description  non-null, non-blank description for the new task
+     * @return             the newly created Task object
+     * @throws IllegalArgumentException if description is null or blank
+     */
+
+    public Task createTask(String description){
+        if(description == null || description.isBlank()){
+            throw new IllegalArgumentException("Description is empty.");
+        }
+        Task newTask = new Task(id++, description);
+        tasks.add(newTask);
+        return newTask;
+    }
+
+    //remove task based on Id
+    public boolean removeTask(int id){
+        for(Task task : tasks){
+            if(task.getId() == id){
+                tasks.remove(id);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //update the task based on id
+    public boolean updateTask(int id, String newDescription){
+        for(Task task : tasks){
+            if(task.getId() == id){
+                task.setDescription(newDescription);
+                return true;
+            }
+        }
+        return  false;
+    }
+
+  //read
+    public List<Task> allTasks(){
+        return new ArrayList<>(tasks);
+    }
+
+    //mark as completed
+    public boolean setCompleted(int id){
+        for(Task task : tasks){
+            if(task.getId() == id){
+                task.setCompleted(true);
+                return true;
+            }
+        }
+        return false;
+    }
+}
